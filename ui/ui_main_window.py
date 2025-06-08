8940b22a8660e37dc7992ad08e9ecea6a32df946
olhaLK <olga.lashuck@gmail.com>
2025-06-08 18:13:45 +0200

    version 002 - stable

--- ui/ui_main_window.py
+++ ui/ui_main_window.py
@@ -15,15 +15,15 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QLineEdit, QMainWindow,
-    QMenuBar, QPushButton, QScrollArea, QSizePolicy,
-    QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLineEdit,
+    QMainWindow, QMenuBar, QPushButton, QScrollArea,
+    QSizePolicy, QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(831, 559)
+        MainWindow.resize(615, 853)
         MainWindow.setMouseTracking(True)
         MainWindow.setStyleSheet(u"background: rgb(251, 255, 237);")
         self.centralwidget = QWidget(MainWindow)
@@ -34,8 +34,8 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
         self.centralwidget.setSizePolicy(sizePolicy)
         self.centralwidget.setMinimumSize(QSize(0, 30))
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
         self.widget = QWidget(self.centralwidget)
         self.widget.setObjectName(u"widget")
         sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
@@ -46,16 +46,18 @@ class Ui_MainWindow(object):
         self.filterMain.setGeometry(QRect(10, 0, 231, 31))
         self.filterMain.setStyleSheet(u"background: white;\n"
 "border: 2 solid #A5D6A7 ;\n"
-"border-radius: 6px;")
+"border-radius: 6px;\n"
+"color: rgb(125, 92, 73);\n"
+"font-size: 14px;")
         self.searchMain = QLineEdit(self.widget)
         self.searchMain.setObjectName(u"searchMain")
         self.searchMain.setGeometry(QRect(330, 0, 241, 31))
         self.searchMain.setStyleSheet(u"background-color: #FFFFFF;\n"
 "border: 2px solid #A5D6A7;\n"
 "padding: 5px;\n"
-"color: #2E7D32;\n"
 "border-radius: 6px;\n"
-"\n"
+"color: rgb(125, 92, 73);\n"
+"font-size: 14px;\n"
 "")
         self.btnSearchMain = QPushButton(self.widget)
         self.btnSearchMain.setObjectName(u"btnSearchMain")
@@ -80,35 +82,36 @@ class Ui_MainWindow(object):
 "")
         self.btnAddMain.setLocale(QLocale(QLocale.English, QLocale.Barbados))
 
-        self.verticalLayout.addWidget(self.widget)
+        self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
 
         self.scrollAreaMain = QScrollArea(self.centralwidget)
         self.scrollAreaMain.setObjectName(u"scrollAreaMain")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.scrollAreaMain.sizePolicy().hasHeightForWidth())
+        self.scrollAreaMain.setSizePolicy(sizePolicy1)
         self.scrollAreaMain.setMinimumSize(QSize(0, 0))
         self.scrollAreaMain.setStyleSheet(u"border: none;")
         self.scrollAreaMain.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 813, 442))
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.scrollAreaWidgetContents.sizePolicy().hasHeightForWidth())
         self.scrollAreaWidgetContents.setSizePolicy(sizePolicy1)
-        self.scrollAreaWidgetContents.setMinimumSize(QSize(800, 0))
+        self.scrollAreaWidgetContents.setMinimumSize(QSize(0, 0))
         self.scrollAreaWidgetContents.setMaximumSize(QSize(16777215, 16777215))
         self.scrollAreaWidgetContents.setMouseTracking(True)
-        self.scrollAreaWidgetContents.setStyleSheet(u"border: 2 solid rgb(255, 170, 127);")
+        self.scrollAreaWidgetContents.setStyleSheet(u"")
         self.scrollAreaMain.setWidget(self.scrollAreaWidgetContents)
 
-        self.verticalLayout.addWidget(self.scrollAreaMain)
+        self.gridLayout.addWidget(self.scrollAreaMain, 1, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.scrollAreaMain.raise_()
         self.widget.raise_()
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 831, 22))
+        self.menubar.setGeometry(QRect(0, 0, 615, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
